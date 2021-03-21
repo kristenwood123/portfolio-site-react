8b1642e9f9e5d8764e981e5fbdba1a4514b2466e
Kristen <kristenwood123@gmail.com>
2021-03-21 15:19:25 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyles } from '../global';
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <GlobalStyles>
+    <div className="App">
+  <Navbar/>
+    </div>
+    </GlobalStyles>
+  );
+}
+
+export default App;
