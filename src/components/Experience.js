da6f07c58af58e0ef019f4645b5bd6ad8a884467
Kristen <kristenwood123@gmail.com>
2021-03-22 13:22:12 -0700

    added jobs data

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Experience = () => {
   return (
     <section className='experience'>
       <h1 className='header__text'>Experience</h1>
+        <hr class='big big-experience'/>
       
     </section>
   )
---
+++ src/experience.js
@@ -0,0 +1,26 @@
+const jobs = [
+  {
+    id: 1,
+    order: 3,
+    title: 'Full Stack Web Developer',
+    dates: 'December 2019 - Present',
+    duties: ['Cake wafer apple pie sweet. Cheesecake liquorice carrot cake chupa chups macaroon powder.', 'Cupcake sugar plum biscuit cupcake dessert cupcake sesame snaps. Candy canes powder jelly pudding tootsie roll.', ' Dessert oat cake sweet roll jujubes jelly-o dragée. Jelly beans apple pie toffee. ' ],
+    company: 'QDesign'
+  },
+    {
+    id: 2,
+    order: 2,
+    title: 'Front-End Web Developer',
+    dates: 'May 2017 - December 2019',
+    duties: ['Fromage stinking bishop cheesy feet. Croque monsieur squirty cheese cow queso goat stinking bishop mozzarella port-salut.', 'Fromage frais the big cheese airedale edam cheesecake mozzarella stilton stinking bishop.', 'Jarlsberg paneer cow macaroni cheese cow emmental squirty cheese macaroni cheese.'],
+    company: 'XWorks'
+  },
+    {
+    id: 3,
+    order: 1,
+    title: 'Engineering Inter',
+    dates: 'January 2017 - May 2017',
+    duties: ['Keffiyeh hella tote bag, everyday carry shaman street art blue bottle cronut pitchfork sustainable narwhal skateboard roof party.', 'Farm-to-table taxidermy venmo vaporware, activated charcoal bespoke lo-fi DIY banh mi post-ironic poutine.', 'Leggings normcore banjo craft beer tofu gastropub, neutra banh mi literally.'],
+    company: 'Redefined Web'
+  }
+]
