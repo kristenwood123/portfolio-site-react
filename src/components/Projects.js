88f67890726a8810699b6936d7b9185f53d335d4
Kristen <kristenwood123@gmail.com>
2021-04-01 16:11:11 -0700

    styled project portion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,13 +14,17 @@ const Projects = () => {
           return (
             <article className="projects__card">
             <div key={id}>
-            <img src={img} style={{ width:'288px', height: '300px'}} alt=""/>
-            <p className='project__number'>{`0${id}.`}</p>
-            <h1 className='project__title'>{title}</h1>
-            <p className='project__text'>{text}</p>
-            <button className='btn2'>{button1}</button>
-            <button className='btn2'>{button2}</button><br></br>
-            <FaGithub/>
+            <img src={img} className='project__img' alt=""/>
+            <div className="project2__container">
+              <p className='project__number'>{`0${id}.`}</p>
+              <h1 className='project__title'>{title}</h1>
+              <p className='project__text'>{text}</p>
+              <div className="project__button__container">
+                <button className='btn2'>{button1}</button>
+                <button className='btn2'>{button2}</button><br></br>
+                </div>
+                <FaGithub className='project__icon'/>
+              </div>
             </div>
           </article>
           )
