7c4a2e1a8a6d114a8163d0d4c0f871cce1ecd35b
Kristen <kristenwood123@gmail.com>
2021-04-08 14:41:09 -0700

    changed project titles

--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     img: './images/projects-2.jpg',
-    title: 'HTML & CSS Project', 
+    title: 'HTML & CSS Project',
     text: 'YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid. Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.',
     button1: 'HTML/CSS',
     button2: 'Javascript'
@@ -10,7 +10,7 @@ const projects = [
   {
     id: 2,
     img: './images/projects-1.jpg',
-    title: 'HTML & CSS Project', 
+    title: 'Javascript & Sass',
     text: 'YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid. Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.',
     button1: 'HTML/CSS',
     button2: 'Javascript'
@@ -18,7 +18,7 @@ const projects = [
   {
     id: 3,
     img: './images/projects-3.jpg',
-    title: 'HTML & CSS Project', 
+    title: 'React, HTML, CSS',
     text: 'YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid. Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.',
     button1: 'HTML/CSS',
     button2: 'Javascript'
@@ -26,7 +26,7 @@ const projects = [
   {
     id: 4,
     img: './images/projects-4.jpg',
-    title: 'HTML & CSS Project', 
+    title: 'React, Bootstrap, HTML, CSS',
     text: 'YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid. Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.',
     button1: 'HTML/CSS',
     button2: 'Javascript'
