43610c503f27bd1e89f0fb01a1684bf602712508
Kristen <kristenwood123@gmail.com>
2021-04-08 13:14:23 -0700

    added projects logic

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import projects from '../projects'
+import React, { useState } from 'react'
+import data from '../projects'
 import { FaGithubSquare, FaShareSquare } from 'react-icons/fa'
 
 const Projects = () => {
+  const [projects, setProjects] = useState(data)
+  const [showProjects, setShowProjects] = useState(true)
+
+  const handleProjects = () => {
+    setProjects([])
+    if (projects.length === 0) {
+      setShowProjects(prevState => !prevState)
+    } else if (showProjects === false) {
+      setProjects(projects)
+    }
+  }
+
   return (
     <section className='projects' id='projects'>
       <div className="container projects__container">
-      <h1 className='header__text'>Featured Projects</h1>
-        <hr className="big"/>
-      
+        <h1 className='header__text'>Featured Projects</h1>
+        <hr className="big" />
+
         {projects.map((project) => {
-          const {id, img, title, text, button1, button2} = project;
+          const { id, img, title, text, button1, button2 } = project;
           return (
             <article className="projects__card" key={id}>
               <div className="project__overlay">
-                <img src={img} className='project__img' alt=""/>    
-                  </div> 
+                <img src={img} className='project__img' alt="" />
+              </div>
               <div className="project2__container">
                 <p className='project__number'>{`0${id}.`}</p>
                 <h1 className='project__title'>{title}</h1>
@@ -23,18 +35,18 @@ const Projects = () => {
                 <div className="project__button__container">
                   <button className='btn2'>{button1}</button>
                   <button className='btn2'>{button2}</button><br></br>
-                  </div>
-                  <div className="project__icon-container">
-                  <FaGithubSquare className='project__icon'/>
-                  <FaShareSquare className='project__icon'/>
+                </div>
+                <div className="project__icon-container">
+                  <FaGithubSquare className='project__icon' />
+                  <FaShareSquare className='project__icon' />
                 </div>
               </div>
-          </article>
+            </article>
           )
         })}
       </div>
       <div className="btn-container">
-      <button className='btn btn-projects'>Projects</button>
+        <button className='btn btn-projects' onClick={handleProjects}>{showProjects ? 'Clear Projects' : 'Show Projects'}</button>
       </div>
     </section>
   )
